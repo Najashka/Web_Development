c7abe6780a6a431f0928da66428f29ee975a6349
Naja <najariski17@gmail.com>
2026-07-09 12:10:21 +0700

    Menambahkan Latihan1 di Folder Praktikum

---
+++ WEEK12/Praktikum/Latihan1.js
@@ -0,0 +1,12 @@
+const mahasiswa = {
+    nama: "Naja Cipuy",
+    nim: "231011xxxx",
+    jurusan: "Teknik Informatika",
+    semester: 3
+};
+
+console.log("===== BIODATA MAHASISWA =====");
+console.log("Nama      :", mahasiswa.nama);
+console.log("NIM       :", mahasiswa.nim);
+console.log("Jurusan   :", mahasiswa.jurusan);
+console.log("Semester  :", mahasiswa.semester);
